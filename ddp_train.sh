883ef0f660569d6cabbc2d27f25ebd630520c883
root <xiongzhitong@gmail.com>
2024-08-22 09:13:28 +0200

    fix bug

--- ddp_train.sh
+++ ddp_train.sh
@@ -1,15 +1,25 @@
-torchrun --nproc-per-node 2  src/open_clip_train/main.py \
-    --save-frequency 1 \
-    --zeroshot-frequency 1 \
+# enter the src folder of the open_clip repository
+
+# specify which GPUs you want to use.
+export CUDA_VISIBLE_DEVICES=0,1
+
+# set the training args
+
+torchrun --nproc_per_node 2 -m src.open_clip_train.main \
+    --batch-size 128 \
+    --precision amp \
+    --workers 8 \
     --report-to tensorboard \
+    --save-frequency 1 \
+    --dataset-type csv \
     --train-data="/home/xshadow/Datasets/remote_clip/csv_file/train/Seg-4.csv"  \
     --val-data="/home/xshadow/Datasets/remote_clip/csv_file/test/ucm_test.csv"  \
     --csv-img-key filename \
     --csv-caption-key title \
-    --warmup 10000 \
-    --batch-size=128 \
-    --lr=1e-3 \
+    --warmup 1000 \
+    --lr=5e-6 \
     --wd=0.1 \
-    --epochs=30 \
-    --workers=8 \
-    --model RN50
+    --epochs=32 \
+    --siglip \
+    --model ViT-B-16-SigLIP \
+    --pretrained webli
